2b5c19e7417ce49fc1c254aab1a5eafea3675af7
Timothy Baldridge <tbaldridge@gmail.com>
2011-08-12 22:25:46 -0500

    Adding project files

---
+++ src/deep_freeze/core.clj
@@ -0,0 +1,1 @@
+(ns deep-freeze.core)
---
+++ test/deep_freeze/test/core.clj
@@ -0,0 +1,6 @@
+(ns deep-freeze.test.core
+  (:use [deep-freeze.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))

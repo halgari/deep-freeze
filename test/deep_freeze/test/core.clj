178f9ac2937a9163104e61613561a97475966d74
Peter Taoussanis <p.taoussanis@gmail.com>
2011-12-15 14:45:01 +0200

    Added a metadata test.

--- test/deep_freeze/test/core.clj
+++ test/deep_freeze/test/core.clj
@@ -16,7 +16,7 @@
    :char       \ಬ
    :string     "ಬಾ ಇಲ್ಲಿ ಸಂಭವಿಸ"
    :key        :key
-   :list      '(1 2 3 4 5 (6 7 8 (9 10)))
+   :list       '(1 2 3 4 5 (6 7 8 (9 10)))
    :vector     [1 2 3 4 5 [6 7 8 [9 10]]]
    :set        #{1 2 3 4 5 #{6 7 8 #{9 10}}}
    :map        {:a 1 :b 2 :c 3 :d {:e 4 :f {:g 5 :h 6 :i 7}}}
@@ -29,12 +29,15 @@
 (defn array-roundtrip [item] (thaw-from-array (freeze-to-array item)))
 
 (deftest test-arrays
-  (is (= testrec (array-roundtrip testrec)))
+  (is (= testrec (array-roundtrip testrec)) "Preserves standard datatypes")
+
+  (is (let [t (with-meta {:a :A} {:metakey :metaval})]
+        (= (meta t) (meta (array-roundtrip t)))) "Preserves metadata")
 
   ;; Equality needs to be munged for STM types
-  (is (let [t (atom  "atom")]  (= @t @(array-roundtrip t))))
-  (is (let [t (ref   "ref")]   (= @t @(array-roundtrip t))))
-  (is (let [t (agent "agent")] (= @t @(array-roundtrip t)))))
+  (is (let [t (atom  "atom")]  (= @t @(array-roundtrip t)))  "Preserves atoms")
+  (is (let [t (ref   "ref")]   (= @t @(array-roundtrip t)))  "Preserves refs")
+  (is (let [t (agent "agent")] (= @t @(array-roundtrip t)))) "Preserves agents")
 
 (deftest test-streaming
   (is false "TODO"))

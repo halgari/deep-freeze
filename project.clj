b3125362a7ffdbc04374d85d8415370e7845ffb9
Timothy Baldridge <tbaldridge@gmail.com>
2011-12-29 23:47:28 -0600

    changing repo location

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject deep-freeze "1.2.2-SNAPSHOT"
   :description "High-performance, robust pure-Clojure de/serializer."
-  :url "https://github.com/ptaoussanis/deep-freeze"
+  :url "https://github.com/halgari/deep-freeze"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.xerial.snappy/snappy-java "1.0.4.1"]]
   :warn-on-reflection true)
